cc633e47f2efc65d8f6d3606123f8c33ff65c015
Joshua Obogbaimhe <jobogbaimhe@gmail.com>
2020-07-10 02:01:42 -0400

    Updated file structure

--- nba/teams/teams_controller.js
+++ nba/teams/teams_controller.js
@@ -2,23 +2,22 @@ const cheerio = require("cheerio")
 const axios = require("axios")
 
 
-async function parseFrachises(data, activity) {
+async function tableParser(data, activity) {
 	try { 
 		const $ = await cheerio.load(data)
 		const index = (activity == "true" || activity == null) ? 0 : 1
 		
-		let arr = [] 
-		$($("tbody").get(index)).find("tr").each((index,element) => {
+		return $($("tbody").get(index)).find("tr").map((index,element) => {
 			let obj = {}
+
 			$(element).children().each((idx,el) => {
 				const k = $(el).attr("data-stat")
 				const v = $(el).text()
 				obj[k] = v
 			})
-			arr.push(obj)
+			
+			return obj
 		})
-		return arr
-	
 	} catch(err) {
 		console.error(err)
 	} 
@@ -29,10 +28,10 @@ module.exports.getTeamIndex = async (req, res) => {
 	try {
 		const { active } = req.query
 		const { data } = await axios.get("https://aws.basketball-reference.com/teams/")
-		parseFrachises(data, active)
+		tableParser(data, active)
 		.then(datum => {
 			res.json({
-				teams: datum
+				teams: datum.toArray()
 			})
 		})
 		.catch(err => {
