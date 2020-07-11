a81a76e45dae23a4d10d5dc055e4483f3d356e33
Joshua Obogbaimhe <jobogbaimhe@gmail.com>
2020-07-11 00:44:06 -0400

    Touching up file structure

---
+++ controller.js
@@ -0,0 +1,18 @@
+const cheerio = require("cheerio")
+
+module.exports.table_parser = async (data) => {
+	try { 
+		const $ = await cheerio.load(data)
+		return $("tr").map((index, row) => {
+			let obj = {}
+			$(row).children().each((idx, field) => {
+				const k = $(field).attr('data-stat')
+				const v = $(field).text()
+				obj[k] = v 
+			})
+			return obj
+		})
+	} catch(err) {
+		console.error(err)
+	} 
+}
--- nba/teams/index.js
+++ nba/teams/index.js
@@ -1,11 +1,22 @@
 const express = require("express")
 const router = express.Router() 
+const { table_parser } = require("../../controller.js")
+const cheerio = require("cheerio")
+const axios = require("axios")
 
-const controller = require("./teams.js")
 
 
-router.get("", (req, res) => {
-	controller.getData(req, res)
+router.get("", async (req, res) => {
+	const { data } = await axios.get("https://www.basketball-reference.com/teams/ATL/2002.html#all_per_game")
+	const $ = await cheerio.load(data)
+	console.log($.html())
+	//console.log($("table").length)
+	const tbl = await table_parser($("tbody").get(0))
+
+	res.json({
+		datum: tbl.toArray() 
+	})
+
 })
 
 
