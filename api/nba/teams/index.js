d664c7f66f2423c2097abdc5fa215a3e6bdca3a2
Joshua Obogbaimhe <jobogbaimhe@gmail.com>
2020-07-15 02:26:50 -0400

    Updated parser and modified tem routes

--- api/nba/teams/index.js
+++ api/nba/teams/index.js
@@ -1,9 +1,23 @@
 const express = require("express")
 const router = express.Router()
-
+const axios = require("axios")
 const parser = require("@parsers")
 
-router.get("", (req, res) => {
+
+router.use("/:franchise", require("./franchise"))
+
+
+router.get("", async (req, res) => {
+	try {
+		const franType = req.query.franchise_type || "active"
+		const { data } = await axios.get(`https://widgets.sports-reference.com/wg.fcgi?css=1&site=bbr&url=%2Fteams%2F&div=div_teams_${franType}`)
+		const result = await parser.tabeParser(data, "tbody")
+		res.json({
+			data: result.toArray()
+		})
+	} catch(err) {
+		res.sendStatus(500)
+	}
 })
 
 
