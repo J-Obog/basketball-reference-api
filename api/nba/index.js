aec1f4bd9e16189e9ca5acd984034f1682cc5743
Joshua Obogbaimhe <jobogbaimhe@gmail.com>
2021-07-16 18:15:43 -0400

    restructuring app

--- api/nba/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-const router = express.Router()
-
-
-router.use("/teams", require("./teams"))
-
-
-
-module.exports = router
--- src/server.js
+++ src/server.js
@@ -1,13 +1,9 @@
-require("module-alias/register")  
-
 const express = require("express")
 const app = express()
 const port = process.env.PORT || 8000
 
 
-app.use("/nba", require("./api/nba"))
-
-
+//app.use("/nba", require("./api/nba"))
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
