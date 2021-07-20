efd1aa7ea20ecd8f0911ccb5fee73b2cebb85604
Joshua Obogbaimhe <jobogbaimhe@gmail.com>
2021-07-20 08:48:10 -0400

    installed html comment stripper module

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-require('dotenv').config(); 
+require("dotenv").config(); 
 const express = require("express"); 
 const app = express(); 
+const api_router = require("./api");  
 const port = process.env.PORT || 8000; 
-
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.json()); 
-
+app.use("/api/v1", api_router); 
 
 app.listen(port, () => {
     console.log(`Server running on port ${port} 🚀🚀🚀`); 
