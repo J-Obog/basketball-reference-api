c907a478e80ca303d05bc9c7f6284e9648c9156a
JObog <jobogbaimhe@gmail.com>
2022-03-12 21:39:29 -0500

    handling environment config on server launch

--- br/app.py
+++ br/app.py
@@ -1,13 +1,16 @@
 from flask import Flask
 from waitress import serve
+from dotenv import load_dotenv
+import os
 
 def create_app():
     app = Flask(__name__)
     return app
 
 if __name__ == '__main__':
+    load_dotenv(override=True)
     app = create_app()
-    HOST = '127.0.0.1'
-    PORT = 8055
-    print(f'Server running on {HOST}:{PORT}')
-    serve(app, host=HOST, port=PORT)
+    host = os.getenv('APP_HOST')
+    port = os.getenv('APP_PORT')
+    print(f'Server running on {host}:{port}')
+    serve(app, host=host, port=port)
