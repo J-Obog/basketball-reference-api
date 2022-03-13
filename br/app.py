bf1965be41e293f8619f9657ecc467b7e8a797a1
JObog <jobogbaimhe@gmail.com>
2022-03-12 22:40:20 -0500

    configuring logger

--- br/app.py
+++ br/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from waitress import serve
 from dotenv import load_dotenv
+import logging
 import os
 
 def create_app():
@@ -8,9 +9,9 @@ def create_app():
     return app
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s [%(name)s] -- %(message)s')
     load_dotenv(override=True)
     app = create_app()
     host = os.getenv('APP_HOST')
     port = os.getenv('APP_PORT')
-    print(f'Server running on {host}:{port}')
     serve(app, host=host, port=port)
